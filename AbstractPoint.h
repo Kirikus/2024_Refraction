49d6a2a06f28b7e49c67977415497cab5ac187c3
0ner <ner8650@gmail.com>
2024-03-04 23:31:03 +0300

    добавлен заголовочник класса абстрактной точки и структура для работы классов

---
+++ AbstractPoint.h
@@ -0,0 +1,12 @@
+#ifndef ABSTRACTPOINT_H
+#define ABSTRACTPOINT_H
+
+#include "sphericalPoint.cpp"
+
+class AbstractPoint
+{
+public:
+    // тут будут методы по общему использованию двух различных классов точек
+};
+
+#endif
---
+++ pointAxes.h
@@ -0,0 +1,10 @@
+#ifndef COORDINATESTRUCT_H
+#define COORDINATESTRUCT_H
+
+struct point{
+    double X;
+    double Y;
+    double H;
+};
+
+#endif // COORDINATESTRUCT_H
