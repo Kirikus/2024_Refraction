b64e78250f79207cd9b66cf423a96843112cc5b2
nuryyevva <nuryyewaaynur03@gmail.com>
2024-03-29 13:44:21 +0300

    feat: Add ExponentialModel class

---
+++ lib/exponential_model.cpp
@@ -0,0 +1,12 @@
+#include "exponential_model.h"
+#include <limits>
+
+double ExponentialModel::N(double h) {
+    return Ns * exp(-(h - hs) / Hb());
+}
+
+double ExponentialModel::Hb() {
+    double hb = 12192;
+    double Nb = 66.65;
+    return (hb - hs) / log(Ns / Nb);
+}
