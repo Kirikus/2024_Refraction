376bd06c30979af43055414a89ac7004ebb39646
nuryyevva <nuryyewaaynur03@gmail.com>
2024-04-05 14:23:51 +0300

    feat: Add class Linear

---
+++ lib/linear.h
@@ -0,0 +1,15 @@
+#ifndef LINEAR_H
+#define LINEAR_H
+
+#include "function1D.h"
+
+class Linear : public Function1D {
+    public:
+        Linear(std::vector<Point> points);
+        virtual double y(double x) override;
+
+    private:
+        std::vector<Point> points;
+};
+
+#endif // LINEAR_H
