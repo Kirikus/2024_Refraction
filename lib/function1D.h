be168d775a3a1b6f98be0509d80d037f53f55041
nuryyevva <nuryyewaaynur03@gmail.com>
2024-04-05 14:23:26 +0300

    feat: Add class Function1D

---
+++ lib/function1D.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTION1D_H
+#define FUNCTION1D_H
+
+#include <vector>
+
+struct Point {
+    double x;
+    double y;
+};
+
+class Function1D {
+    public:
+        virtual double y(double x) = 0;
+};
+
+#endif // FUNCTION1D_H
